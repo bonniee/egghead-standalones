514e73eba3ab35022af97b17b6953325edb0aee6
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2016-11-04 19:47:39 -0400

    PlatformSpecific lesson

---
+++ Tutorial/PlatformSpecific/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Hello from './Hello';
+
+export default class Tutorial extends Component {
+  render() {
+    return (
+      <Hello/>
+    );
+  }
+}
--- Tutorial/index.android.js
+++ Tutorial/index.android.js
@@ -3,8 +3,9 @@ import {
   AppRegistry
 } from 'react-native';
 
+// Pick which lesson you want
 import DebuggingTutorial from './debugging';
+import PlatformSpecific from './PlatformSpecific';
 
 AppRegistry.registerComponent(
-  'Tutorial', () => DebuggingTutorial
-);
+  'Tutorial', () => PlatformSpecific);
