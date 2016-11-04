0df5cbd679d57e94829cc08eefd55a5f0765f2ee
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2016-11-03 21:56:24 -0400

    Tutorial

---
+++ Tutorial/index.android.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import DebuggingTutorial from './debugging';
+
+AppRegistry.registerComponent(
+  'Tutorial', () => DebuggingTutorial
+);
---
+++ Tutorial/index.ios.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import DebuggingTutorial from './debugging';
+
+AppRegistry.registerComponent(
+  'Tutorial', () => DebuggingTutorial);
