6c488014145aca72d4379c7a9bbb006174c3996d
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2016-11-06 10:52:16 -0500

    working

--- Tutorial/video-example.js
+++ Tutorial/video-example.js
@@ -5,29 +5,29 @@ import {
   View
 } from 'react-native';
 
+import Video from 'react-native-video';
+
 export default class VideoExample extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hello, world
-        </Text>
-      </View>
+      <Video source={require('./assets/clouds.mp4')}
+             muted={true}                  // Mutes the audio entirely.
+             paused={false}                 // Pauses playback entirely.
+             resizeMode="cover"             // Fill the whole screen at aspect ratio.
+             repeat={true}                  // Repeat forever.
+             style={styles.backgroundVideo} />
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   }
 });
 
